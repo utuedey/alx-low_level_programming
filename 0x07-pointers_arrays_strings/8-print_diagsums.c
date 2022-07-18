367cd62563850c8829382c9b613a125cdcbb16d1
utuedey <utuedeyejosph@gmail.com>
2022-07-18 15:29:42 -0700

    updated 8-print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,7 +17,7 @@ sum1 = 0;
 for (i = 0; i < size; i++)
 {
 sum += a[(size * i) + i];
-sum1 += a[(size * (1 + 1)) - (i + 1)];
+sum1 += a[(size * (i + 1)) - (i + 1)];
 }
 printf("%d, %d\n", sum, sum1);
 }
